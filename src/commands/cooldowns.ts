32d5ebf3f9329c722b3c8b5e00403ead9aa2feb0
Skylafalls <triplettran56@gmail.com>
2025-11-10 10:19:39 -0500

    chore: add license headers in other places
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- src/commands/cooldowns.ts
+++ src/commands/cooldowns.ts
@@ -1,3 +1,21 @@
+/**
+ * @license
+ * fg-sparky-bot - Guess the FG number based on its symbol
+ * Copyright (C) 2025 Skylafalls
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 import { Collection, type CommandInteraction, MessageFlags } from "discord.js";
 import { Logger } from "../utils/logger";
 import type { ChatInputCommand, Command } from "./types";
