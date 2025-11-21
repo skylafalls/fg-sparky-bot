7ec85be51df3241829b0598e88198b47aeec61df
Skylafalls <triplettran56@gmail.com>
2025-11-21 11:20:15 -0500

    feat(users): populate user entities values
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- src/entities/user-profile.ts
+++ src/entities/user-profile.ts
@@ -16,22 +16,40 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
 
 /**
  * This is a person's user profile.
  */
 @Entity()
-export class UserProfile {
+export class UserProfile extends BaseEntity {
   /**
    * The discord user id for the profile.
    */
-  @PrimaryGeneratedColumn("identity")
-  user_id = 0n;
+  @PrimaryColumn("identity")
+  id = "0";
 
   /**
    * The current terminus token count for the user.
    */
   @Column("integer")
   tokens = 0;
+
+  /**
+   * Array of achievements the user has, pointed by achievement id.
+   */
+  @Column()
+  achievements: number[] = [];
+
+  /**
+   * Array of secret achievements the user has, pointed by their id.
+   */
+  @Column()
+  secret_achievements: number[] = [];
+
+  /**
+   * Array of unique numbers the player has guessed, by the number's UUID.
+   */
+  @Column()
+  unique_guessed: string[] = [];
 }
