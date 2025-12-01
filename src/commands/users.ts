8761d77b10122434cf6b944199a94e75598c4b4c
Skylafalls <triplettran56@gmail.com>
2025-11-30 20:11:49 -0500

    feat(profiles): add user leaderboard subcommand
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -4,9 +4,25 @@
  * Copyright (C) 2025 Skylafalls
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
-import { ApplicationCommandOptionType, type Client, type CommandInteraction } from "discord.js";
+import { ApplicationCommandOptionType, type Client, type CommandInteraction, type User as DiscordUser } from "discord.js";
+import { UserProfile } from "../entities/user-profile.ts";
 import { getUser } from "../utils/user.ts";
 import type { Command } from "./types.ts";
+
+function ordinalOf(number: number): `${number}${"st" | "nd" | "rd" | "th"}` {
+  const j = number % 10,
+    k = number % 100;
+  if (j === 1 && k !== 11) {
+    return `${number}st`;
+  }
+  if (j === 2 && k !== 12) {
+    return `${number}nd`;
+  }
+  if (j === 3 && k !== 13) {
+    return `${number}rd`;
+  }
+  return `${number}th`;
+}
 
 const User: Command = {
   async run(client: Client, interaction: CommandInteraction): Promise<void> {
@@ -23,6 +39,29 @@ const User: Command = {
         } else {
           await interaction.reply("sorry, fg sparky bot doesn't have data for this user");
         }
+        return;
+      }
+      case "leaderboard": {
+        const users = await UserProfile.find({ order: { tokens: "DESC" }, select: ["id", "tokens"] });
+        const discordUsers: DiscordUser[] = await Promise.all(
+          users.map(profile => client.users.fetch(profile.id)),
+        );
+        const content = `\
+        # User leaderboard: \n \
+        ${users.map((user, index) => {
+          if (index > 14) return "no";
+          const position = ordinalOf(index + 1);
+          // Sometimes an IIFE looks better then chaining ternaries
+          const header = ((index) => {
+            if (index === 0) return "##";
+            if (index === 1) return "###";
+            return "";
+          })(index);
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          return `${header} ${position}: ${discordUsers[index]!.displayName} (${user.tokens.toString()} tokens)`;
+        }).filter(value => value !== "no").join("\n")}
+        `;
+        await interaction.reply({ content });
         return;
       }
       default: {
@@ -43,6 +82,10 @@ const User: Command = {
       type: ApplicationCommandOptionType.User,
       required: true,
     }],
+  }, {
+    name: "leaderboard",
+    description: "Show who has the most terminus tokens",
+    type: ApplicationCommandOptionType.Subcommand,
   }],
 };
 
