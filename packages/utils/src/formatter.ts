93e6b3e53f7a0cab1590e700654c599d9e687c65
Skylafalls <triplettran56@gmail.com>
2025-12-30 11:06:56 -0500

    iniital structure
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

---
+++ packages/utils/src/formatter.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * fg-sparky-bot - Guess the FG number based on its symbol
+ * Copyright (C) 2025 Skylafalls
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+export const loginFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat("en-US", { dateStyle: "long", timeStyle: "short" });
+export const loggerFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat("en-US", { dateStyle: "short", timeStyle: "medium" });
+
+/**
+ * Returns a percentage of the `number` value.
+ * @param number The number to format.
+ * @param precision The amount of decimal digits to show.
+ * @returns The percentage.
+ */
+export function formatPercent(number: number, precision = 1): string {
+  return `${(number * 100).toFixed(precision)}%`;
+}
+
+/**
+ * Takes an array of strings and joins them together with a different string.
+ * Useful for proper formatting.
+ */
+export function joinStringArray(array: string[], joiner = "\n"): string {
+  return array.filter(Boolean).join(joiner);
+}
---
+++ packages/utils/src/index.ts
@@ -0,0 +1,10 @@
+export { Command } from "commander";
+export * from "comptime.ts";
+export { Cron, type Logger as CronLogger } from "cronbake";
+export * from "rust-optionals";
+export * from "./assert.ts";
+export * from "./constants.ts";
+export * from "./formatter.ts";
+export * from "./logger.ts";
+export * from "./numbers.ts";
+
