fc415b25d90bdfc639f63aa313a693bf9d5c6442
Skylafalls <triplettran56@gmail.com>
2026-01-07 15:30:42 -0500

    fix that script
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- scripts/ensure-images-exist.ts
+++ scripts/ensure-images-exist.ts
@@ -1,46 +1,16 @@
 import numbers from "../numbers/numbers.json" with { type: "json" };
+import { NumberInfo } from "@fg-sparky/server";
 
-const easies = await Promise.allSettled(numbers.easy.map(async (value) => {
-  try {
-    await Bun.file(value.image).bytes();
-    return Object.assign(value, { missingImage: false });
-  } catch {
-    return Object.assign(value, { missingImage: true });
-  }
+const entries = await Promise.allSettled(numbers
+  .map(value => NumberInfo.parse(value))
+  .map(async (value) => {
+    try {
+      await Bun.file(value.image).bytes();
+      return Object.assign(value, { missingImage: false });
+    } catch {
+      return Object.assign(value, { missingImage: true });
+    }
 }));
 
-const medium = await Promise.allSettled(numbers.medium.map(async (value) => {
-  try {
-    await Bun.file(value.image).bytes();
-    return Object.assign(value, { missingImage: false });
-  } catch {
-    return Object.assign(value, { missingImage: true });
-  }
-}));
-
-const hard = await Promise.allSettled(numbers.hard.map(async (value) => {
-  try {
-    await Bun.file(value.image).bytes();
-    return Object.assign(value, { missingImage: false });
-  } catch {
-    return Object.assign(value, { missingImage: true });
-  }
-}));
-
-const legendary = await Promise.allSettled(numbers.legendary.map(async (value) => {
-  try {
-    await Bun.file(value.image).bytes();
-    return Object.assign(value, { missingImage: false });
-  } catch {
-    return Object.assign(value, { missingImage: true });
-  }
-}));
-
-console.log("========== EASIES ==========");
-console.log(easies.filter(value => value.status === "fulfilled" && value.value.missingImage).map(value => value.value));
-console.log("========== MEDIUMS ==========");
-console.log(medium.filter(value => value.status === "fulfilled" && value.value.missingImage));
-console.log("========== HARD ==========");
-console.log(hard.filter(value => value.status === "fulfilled" && value.value.missingImage));
-console.log("========== LEGENDARIES ==========");
-console.log(legendary.filter(value => value.status === "fulfilled" && value.value.missingImage));
+console.log("========== NUMBERS WITHOUT IMAGES ==========");
+console.log(entries.filter(value => value.status === "fulfilled").filter(value => value.value.missingImage).map(value => value.value));
